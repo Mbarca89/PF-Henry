45bc811e8e823ff407c3016ef467288d7cdd7d1d
Telkens <stivenmg77@gmail.com>
2023-06-27 19:26:24 -0500

    Filtro de categorias agregado

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,142 +1,210 @@
-import styles from './Filters.module.css';
-import { FcShipped, FcRating, FcHome, FcAdvertising, FcMoneyTransfer, FcPositiveDynamic } from 'react-icons/fc';
-import { GoChevronRight } from 'react-icons/go';
-import {MdAttachMoney} from 'react-icons/md'
-import { useAppDispatch, useAppSelector } from '../../redux/store';
-import { useNavigate } from 'react-router-dom';
-import { setBody, setPage } from '../../redux/slices/productsSlice';
-import { ChangeEvent, useState, useEffect } from 'react';
-import { fecthProducts } from '../../redux/utils/fetchProducts';
-import { Body } from '../../types';
+import styles from "./Filters.module.css";
+import {
+  FcShipped,
+  FcRating,
+  FcHome,
+  FcAdvertising,
+  FcMoneyTransfer,
+  FcPositiveDynamic,
+} from "react-icons/fc";
+import { GoChevronRight } from "react-icons/go";
+import { MdAttachMoney } from "react-icons/md";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
+import { useNavigate } from "react-router-dom";
+import { setBody, setPage } from "../../redux/slices/productsSlice";
+import { ChangeEvent, useState, useEffect } from "react";
+import { fecthProducts } from "../../redux/utils/fetchProducts";
+import { Body } from "../../types";
+import { notifyError } from "../../components/Toaster/Toaster.js";
+import axios from "axios";
+import { setUser } from "../../redux/slices/userSlice";
 
 const Filters = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { body } = useAppSelector(state => state.products);
-  const urlName = useAppSelector(state => state.products.urlName)
+  const { body } = useAppSelector((state) => state.products);
+  const urlName = useAppSelector((state) => state.products.urlName);
+
+  interface Product {
+    _id: string;
+  }
+
+  interface Category {
+    categoryName: string;
+    products: Product[];
+    id: string;
+  }
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [userName, setUserName] = useState("");
   const [stateFiltered, setStateFiltered] = useState<Body>({
     sort: {
-      price: { isSorted: false, order: 'asc' },
-      sales: { isSorted: false, order: 'asc' },
-      relevant: { isSorted: false, order: 'asc' },
+      price: { isSorted: false, order: "asc" },
+      sales: { isSorted: false, order: "asc" },
+      relevant: { isSorted: false, order: "asc" },
     },
     freeShipping: false,
     hasDiscount: false,
-    category: '',
-    minPrice: '',
-    maxPrice: Infinity
+    category: "",
+    minPrice: "",
+    maxPrice: Infinity,
   });
   useEffect(() => {
-    setStateFiltered(body)
-  }, [body])
+    setStateFiltered(body);
+  }, [body]);
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, checked } = event.target;
     setStateFiltered((prevState) => {
       return {
         ...prevState,
-        [name]: checked
+        [name]: checked,
       };
     });
-
   };
   const handlePrice = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setStateFiltered((prevState) => {
       return {
         ...prevState,
-        [name]: value
+        [name]: value,
       };
     });
   };
   const handleChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = event.target;
-    if(name === 'price'){
-      setStateFiltered(
-        {
-          ...stateFiltered,
-          sort:{
-            ...stateFiltered.sort,
-            price:{
-              isSorted:true,
-              order: value
-            }
-          }            
-        })        
-  }
-  else if(name === 'sales'){
-    setStateFiltered(
-      {
+    if (name === "price") {
+      setStateFiltered({
         ...stateFiltered,
-        sort:{
+        sort: {
           ...stateFiltered.sort,
-          sales:{
-            isSorted:true,
-            order: value
-          }
-        }            
-      })        
-}
-if(name === 'relevant'){
-  setStateFiltered(
-    {
-      ...stateFiltered,
-      sort:{
-        ...stateFiltered.sort,
-        relevant:{
-          isSorted:true,
-          order: value
-        }
-      }            
-    })        
-}
+          price: {
+            isSorted: true,
+            order: value,
+          },
+        },
+      });
+    } else if (name === "sales") {
+      setStateFiltered({
+        ...stateFiltered,
+        sort: {
+          ...stateFiltered.sort,
+          sales: {
+            isSorted: true,
+            order: value,
+          },
+        },
+      });
+    }
+    if (name === "relevant") {
+      setStateFiltered({
+        ...stateFiltered,
+        sort: {
+          ...stateFiltered.sort,
+          relevant: {
+            isSorted: true,
+            order: value,
+          },
+        },
+      });
+    }
   };
   const handleApplyFilters = () => {
     dispatch(setBody(stateFiltered));
-    dispatch(fecthProducts({
-      page: '1',
-      name: urlName,
-      body: stateFiltered
-    }))
+    dispatch(
+      fecthProducts({
+        page: "1",
+        name: urlName,
+        body: stateFiltered,
+      })
+    );
   };
 
   const handleResetFilters = () => {
-    dispatch(setBody({
-      sort: {
-        price: { isSorted: false, order: 'asc' },
-        sales: { isSorted: false, order: 'asc' },
-        relevant: { isSorted: true, order: 'desc' },
-      },
-      freeShipping: false,
-      hasDiscount: false,
-      category: '',
-      minPrice: '',
-      maxPrice: Infinity
-    }));
-    dispatch(setPage('1'))
-    dispatch(fecthProducts({
-      page: '1',
-      name: urlName,
-      body: {
+    dispatch(
+      setBody({
         sort: {
-          price: { isSorted: false, order: 'asc' },
-          sales: { isSorted: false, order: 'asc' },
-          relevant: { isSorted: true, order: 'desc' },
+          price: { isSorted: false, order: "asc" },
+          sales: { isSorted: false, order: "asc" },
+          relevant: { isSorted: true, order: "desc" },
         },
         freeShipping: false,
         hasDiscount: false,
-        category: '',
-        minPrice: '',
-        maxPrice: Infinity
-      }
-    }))
+        category: "",
+        minPrice: "",
+        maxPrice: Infinity,
+      })
+    );
+    dispatch(setPage("1"));
+    dispatch(
+      fecthProducts({
+        page: "1",
+        name: urlName,
+        body: {
+          sort: {
+            price: { isSorted: false, order: "asc" },
+            sales: { isSorted: false, order: "asc" },
+            relevant: { isSorted: true, order: "desc" },
+          },
+          freeShipping: false,
+          hasDiscount: false,
+          category: "",
+          minPrice: "",
+          maxPrice: Infinity,
+        },
+      })
+    );
   };
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/categories");
+        setCategories(response.data);
+      } catch (error: any) {
+        notifyError(error.response.data);
+      }
+    };
+    fetchCategories();
+    if (document.cookie) {
+      const tokenCookie = document.cookie
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("token="));
+      const token = tokenCookie?.split("=")[1];
+      if (token) localStorage.setItem("token", token);
+      const userCookie = document.cookie
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("user="));
+      if (userCookie) {
+        const userJSON = decodeURIComponent(userCookie.split("=")[1]);
+        const userOk = JSON.parse(userJSON);
+        localStorage.setItem("userData", JSON.stringify(userOk));
+        dispatch(setUser(userOk));
+      }
+      document.cookie =
+        "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      window.location.reload();
+      if (token) navigate("/home");
+    }
+    const storedUserData = localStorage.getItem("userData");
+    if (storedUserData) {
+      const storedUserDataOk = JSON.parse(storedUserData);
+      setUserName(storedUserDataOk.name);
+    }
+  }, []);
+  const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    setStateFiltered({
+      ...stateFiltered,
+      category: event.target.value,
+    });
+    console.log(event.target.value);
+    console.log(stateFiltered);
+  };
   return (
     <div className={styles.filters_container}>
-      <div className={styles.filters_option} onClick={() => navigate('/')}>
+      <div className={styles.filters_option} onClick={() => navigate("/")}>
         <FcHome size={25} />
         <p>Inicio</p>
-        <GoChevronRight size={25} style={{ color: 'black' }} />
+        <GoChevronRight size={25} style={{ color: "black" }} />
       </div>
       <div className={styles.filters_option}>
         <FcShipped size={25} />
@@ -149,7 +217,13 @@ if(name === 'relevant'){
             checked={stateFiltered.freeShipping}
           />
           <span className={styles.slider}>
-            <svg className={styles.slider_icon} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation">
+            <svg
+              className={styles.slider_icon}
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+            >
               <path fill="none" d="m4 16.5 8 8 16-16"></path>
             </svg>
           </span>
@@ -171,14 +245,42 @@ if(name === 'relevant'){
         <FcAdvertising size={25} />
         <p>Ofertas</p>
         <label className={styles.switch}>
-          <input type="checkbox" onChange={handleChange} checked={stateFiltered.hasDiscount} name="hasDiscount" />
+          <input
+            type="checkbox"
+            onChange={handleChange}
+            checked={stateFiltered.hasDiscount}
+            name="hasDiscount"
+          />
           <span className={styles.slider}>
-            <svg className={styles.slider_icon} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation">
+            <svg
+              className={styles.slider_icon}
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+            >
               <path fill="none" d="m4 16.5 8 8 16-16"></path>
             </svg>
           </span>
         </label>
       </div>
+
+      <div className={styles.categorySelect}>
+        <select
+          className={styles.customSelect}
+          onChange={(event) => handleSelect(event)}
+        >
+          <option>Categorias</option>
+          {categories.map((category) => {
+            return (
+              <option key={category.id} value={category.id}>
+                {category.categoryName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
       <div className={styles.filters_option}>
         <FcRating size={25} />
         <select name="sales" onChange={handleChangeSelect}>
@@ -201,14 +303,32 @@ if(name === 'relevant'){
         </select>
       </div>
       <div className={styles.price_filters_option}>
-        <MdAttachMoney size={25}/>
-          <div className={styles.range_prize_container}>
-            <input type='number' min='0' placeholder='Precio mínimo' value={stateFiltered.minPrice} name='minPrice' onChange={handlePrice}/>
-            <input type='number' min='0' placeholder='Precio máximo' value={stateFiltered.maxPrice} name='maxPrice' onChange={handlePrice}/>
-          </div>
+        <MdAttachMoney size={25} />
+        <div className={styles.range_prize_container}>
+          <input
+            type="number"
+            min="0"
+            placeholder="Precio mínimo"
+            value={stateFiltered.minPrice}
+            name="minPrice"
+            onChange={handlePrice}
+          />
+          <input
+            type="number"
+            min="0"
+            placeholder="Precio máximo"
+            value={stateFiltered.maxPrice}
+            name="maxPrice"
+            onChange={handlePrice}
+          />
+        </div>
       </div>
-      <button className={styles.btn} onClick={handleApplyFilters}>Aplicar filtros</button>
-      <button className={styles.btn} onClick={handleResetFilters}>Limpiar filtros</button>
+      <button className={styles.btn} onClick={handleApplyFilters}>
+        Aplicar filtros
+      </button>
+      <button className={styles.btn} onClick={handleResetFilters}>
+        Limpiar filtros
+      </button>
     </div>
   );
 };
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,143 +1,206 @@
-import styles from './NavBar.module.css'
-import { NavLink } from 'react-router-dom';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { BiLogIn } from 'react-icons/bi';
-import { CgProfile } from 'react-icons/cg'
-import { useLocation, useNavigate } from 'react-router-dom';
-import SearchBar from '../SearchBar/SearchBar';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { setUser } from '../../redux/slices/userSlice';
-import { useAppDispatch } from '../../redux/store';
-import {notifyError} from "../../components/Toaster/Toaster.js";
-
+import styles from "./NavBar.module.css";
+import { Body } from "../../types";
+import { NavLink } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BiLogIn } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { useLocation, useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import axios from "axios";
+import { ChangeEvent, useEffect, useState } from "react";
+import { setUser } from "../../redux/slices/userSlice";
+import { useAppDispatch } from "../../redux/store";
+import { notifyError } from "../../components/Toaster/Toaster.js";
+import { setBody } from "../../redux/slices/productsSlice.js";
+import { fecthProducts } from "../../redux/utils/fetchProducts.js";
 
 const NavBar = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-    const dispatch = useAppDispatch()
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
+  interface Product {
+    _id: string;
+  }
 
-    interface Product {
-        _id: string;
-    }
+  interface Category {
+    categoryName: string;
+    products: Product[];
+    id: string;
+  }
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [userName, setUserName] = useState("");
+  const [stateFiltered, setStateFiltered] = useState<Body>({
+    sort: {
+      price: {
+        isSorted: false,
+        order: "asc",
+      },
+      sales: {
+        isSorted: false,
+        order: "asc",
+      },
+      relevant: {
+        isSorted: true,
+        order: "desc",
+      },
+    },
+    freeShipping: false,
+    hasDiscount: false,
+    category: "",
+    minPrice: "",
+    maxPrice: Infinity,
+  });
 
-    interface Category {
-        categoryName: string;
-        products: Product[];
-        id: string;
-    }
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [showCategories, setShowCategories] = useState(false)
-    const [userName, setUserName] = useState("");
-
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-                const response = await axios.get('http://185.253.153.34:3001/categories');
-                setCategories(response.data);
-
-            } catch (error:any) {
-                notifyError(error.response.data)
-            }
-        }
-        fetchCategories();
-        if (document.cookie) {
-            const tokenCookie = document.cookie
-                .split(';')
-                .find(cookie => cookie.trim().startsWith('token='));
-            const token = tokenCookie?.split('=')[1]
-            if (token) localStorage.setItem('token', token)
-            const userCookie = document.cookie
-                .split(';')
-                .find(cookie => cookie.trim().startsWith('user='));
-            if (userCookie) {
-                const userJSON = decodeURIComponent(userCookie.split('=')[1]);
-                const userOk = JSON.parse(userJSON)
-                localStorage.setItem('userData', JSON.stringify(userOk))
-                dispatch(setUser(userOk))
-            }
-            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-            document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-            window.location.reload()
-            if(token) navigate('/home')
-        }
-        const storedUserData = localStorage.getItem("userData");
-        if (storedUserData) {            
-            const storedUserDataOk = JSON.parse(storedUserData)
-            setUserName(storedUserDataOk.name);
-        }
-        
-    }, [])
-
-   
-
-    const showCategoriesHandler = () => {
-        setShowCategories(!showCategories)
-    }
-
-    const handleLogout = () => {
-        localStorage.removeItem("userData");
-        localStorage.removeItem("token");
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        window.location.reload();
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/categories");
+        setCategories(response.data);
+      } catch (error: any) {
+        notifyError(error.response.data);
+      }
     };
+    fetchCategories();
+    if (document.cookie) {
+      const tokenCookie = document.cookie
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("token="));
+      const token = tokenCookie?.split("=")[1];
+      if (token) localStorage.setItem("token", token);
+      const userCookie = document.cookie
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("user="));
+      if (userCookie) {
+        const userJSON = decodeURIComponent(userCookie.split("=")[1]);
+        const userOk = JSON.parse(userJSON);
+        localStorage.setItem("userData", JSON.stringify(userOk));
+        dispatch(setUser(userOk));
+      }
+      document.cookie =
+        "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      window.location.reload();
+      if (token) navigate("/home");
+    }
+    const storedUserData = localStorage.getItem("userData");
+    if (storedUserData) {
+      const storedUserDataOk = JSON.parse(storedUserData);
+      setUserName(storedUserDataOk.name);
+    }
+  }, []);
 
-    return (
-        categories &&
-        <div className={styles.navbar_container}>
-            <img src="/src/assets/logook.png" className={styles.navbar_logo} alt="logo" onClick={() => navigate('/home')} />
-            <div className={styles.navbar_items}>
-                <div className={styles.navbar_options}>
-                    <div className={styles.categorySelect}>
-                        <button style={{ cursor: 'pointer' }} className={showCategories ? styles.navbar_button_active : styles.navbar_button} onClick={showCategoriesHandler}>Categorias</button>
-                        <div className={styles.customSelect} style={showCategories ? { opacity: 1 } : { opacity: 0 }}>
-                            {
-                                categories.map(category => {
-                                    return (
-                                        <div key={category.id}>{category.categoryName}</div>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
+  const handleLogout = () => {
+    localStorage.removeItem("userData");
+    localStorage.removeItem("token");
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.reload();
+  };
+  const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    setStateFiltered({
+      ...stateFiltered,
+      category: event.target.value,
+    });
+    dispatch(setBody(stateFiltered));
+    dispatch(
+      fecthProducts({
+        page: "1",
+        name: "",
+        body: stateFiltered,
+      })
+    );
+    navigate("/products");
+  };
+  return (
+    categories && (
+      <div className={styles.navbar_container}>
+        <img
+          src="/src/assets/logook.png"
+          className={styles.navbar_logo}
+          alt="logo"
+          onClick={() => navigate("/home")}
+        />
+        <div className={styles.navbar_items}>
+          <div className={styles.navbar_options}>
+            {pathname !== "/products" && (
+              <div className={styles.categorySelect}>
+                <select
+                  className={styles.customSelect}
+                  onChange={(event) => handleSelect(event)}
+                >
+                  <option>Categorias</option>
+                  {categories.map((category) => {
+                    return (
+                      <option key={category.id} value={category.id}>
+                        {category.categoryName}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+            )}
+            <NavLink
+              to="/products"
+              className={
+                pathname === "/products"
+                  ? styles.navbar_button_active
+                  : styles.navbar_button
+              }
+            >
+              Productos
+            </NavLink>
+            <NavLink
+              className={
+                pathname === "/about"
+                  ? styles.navbar_button_active
+                  : styles.navbar_button
+              }
+              to="/about"
+            >
+              Sobre Nosotros
+            </NavLink>
+          </div>
+          <div className={styles.navbar_search}>
+            <SearchBar />
+          </div>
 
-                    <NavLink to='/products' className={pathname === '/products' ? styles.navbar_button_active : styles.navbar_button}>Productos</NavLink>
-                    <NavLink className={pathname === '/about' ? styles.navbar_button_active : styles.navbar_button} to='/about'>Sobre Nosotros</NavLink>
-                </div>
-                <div className={styles.navbar_search}>
-                    <SearchBar />
-                </div>
-
-                <div className={styles.navbar_icons}>
-                    {userName && <p>{`Hola ${userName}!`}</p>}
-                    {userName && <div className={styles.navbar_icon} onClick={() => navigate('/myprofile')}>
-                            <CgProfile size={25} />
-                            <span>Mi perfil</span>
-                        </div>}
-                    {userName ? (
-                        <div className={styles.navbar_icon} onClick={handleLogout}>
-                            <BiLogIn size={25} />
-                            <span>Salir</span>
-                        </div>
-                    ) : (
-                        <div
-                            className={styles.navbar_icon}
-                            onClick={() => navigate("/login")}
-                        >
-                            <BiLogIn size={25} />
-                            <span>Ingresar</span>
-                        </div>
-                    )}
-                    <div className={styles.navbar_icon} onClick={() => navigate('/cart')}>
-                        <AiOutlineShoppingCart size={25} />
-                        <span>Carrito</span>
-                    </div>
-                </div>
-
+          <div className={styles.navbar_icons}>
+            {userName && <p>{`Hola ${userName}!`}</p>}
+            {userName && (
+              <div
+                className={styles.navbar_icon}
+                onClick={() => navigate("/myprofile")}
+              >
+                <CgProfile size={25} />
+                <span>Mi perfil</span>
+              </div>
+            )}
+            {userName ? (
+              <div className={styles.navbar_icon} onClick={handleLogout}>
+                <BiLogIn size={25} />
+                <span>Salir</span>
+              </div>
+            ) : (
+              <div
+                className={styles.navbar_icon}
+                onClick={() => navigate("/login")}
+              >
+                <BiLogIn size={25} />
+                <span>Ingresar</span>
+              </div>
+            )}
+            <div
+              className={styles.navbar_icon}
+              onClick={() => navigate("/cart")}
+            >
+              <AiOutlineShoppingCart size={25} />
+              <span>Carrito</span>
             </div>
+          </div>
         </div>
+      </div>
     )
-}
+  );
+};
 export default NavBar;
