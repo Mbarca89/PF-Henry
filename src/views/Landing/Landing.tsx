0e926cd52f3c74cfc3119e972bf6d65f4511551b
Telkens <stivenmg77@gmail.com>
2023-06-29 11:47:01 -0500

    Importación del logo landing

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -1,12 +1,13 @@
 import style from './Landing.module.css';
 import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logook.png'
 
 const Landing = () => {
     const navigate = useNavigate();
     return (
       <div className={style.container_background}>
         <div className={style.container_landing}>
-            <img src="/src/assets/logook.png" alt="logo" className={style.container_logo} />
+            <img src={logo} alt="logo" className={style.container_logo} />
           <div className={style.container_description}>
               <h1>Tu belleza natural merece productos naturales.</h1>
               {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut cupiditate quis, culpa possimus sapiente exercitationem, tempore autem nesciunt dolores, sequi explicabo ipsam quisquam totam soluta architecto. Iusto, blanditiis voluptatibus. Vitae.</p> */}
