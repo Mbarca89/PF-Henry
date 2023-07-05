b748c2742c6ebf3a62d2971dfeac6d5abaf20542
Telkens <stivenmg77@gmail.com>
2023-07-05 18:14:50 -0500

    About finished

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,56 +1,34 @@
 import styles from './About.module.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import {REACT_APP_SERVER_URL} from '../../../config.ts'
 
 const About = () => {
 
-  const [userName, setUserName] = useState("");
-
-  useEffect(() => {
-    const storedUserName = localStorage.getItem("userData");
-    if (storedUserName) {
-      const storedUserNameOk = JSON.parse(storedUserName)
-      setUserName(storedUserNameOk);
-    }
-  }, []);
-
-    const productList = [
-        {
-          itemName: "crema para manos",
-          unityPrice: 3700,
-          quantity: 2,
-          total: 7400
-        },
-        {
-          itemName: "crema de enjuague",
-          unityPrice: 3300,
-          quantity: 3,
-          total: 9900
-        }
-      ]
-    
-    const checkoutOk = async () => {
-        const {data} = await axios.post(`${REACT_APP_SERVER_URL}/checkout/create-order`, {productList})
-        window.location.href = data.init_point
-    }
-
-
-    if (userName) {
-    return (
-        <div className={styles.about_container}>
-            <h1>Sobre nosotros</h1>
-            <button id="checkout" onClick={checkoutOk}>
-                Pay
-            </button>
-        </div>
-    )
-    } else {
-      return (
-        <div>
-          <span>Please login</span>
-        </div>
-      );
-    }
+  return (
+    <div className={styles.about_container}
+    style={{
+      borderRadius: "10px",
+      boxShadow: "0 6px 10px rgba(0, 0, 0, 0.5)",
+    }}
+    >
+    <img src="/src/assets/logook.png" alt="" />
+    <div className={styles.about_item}>
+      <p>
+        Somos una empresa dedicada a ofrecer productos naturales de alta calidad que promueven el bienestar y la salud. Nos enorgullece ofrecer una amplia gama de productos elaborados con ingredientes naturales cuidadosamente seleccionados, libres de químicos y aditivos artificiales.
+      </p>
+    </div>
+    <div className={styles.horizontalLine}></div>
+    <div className={styles.about_item}>
+      <h3>Misión</h3>
+      <p>
+        Nuestra misión es proporcionar a nuestros clientes productos naturales que mejoren su calidad de vida y promuevan un estilo de vida saludable. Nos esforzamos por ser líderes en la industria de productos naturales, ofreciendo productos de alta calidad y brindando un excelente servicio al cliente.
+      </p>
+    </div>
+    <div className={styles.about_item}>
+      <h3>Visión</h3>
+      <p>
+        Nuestra visión es ser reconocidos como la marca líder en productos naturales, conocida por nuestra calidad, innovación y compromiso con la salud y el bienestar. Nos esforzamos por expandir nuestra oferta de productos, llegar a más personas y ser un referente en el cuidado natural del cuerpo y la mente.
+      </p>
+    </div>
+  </div>
+  )
 }
 export default About;
